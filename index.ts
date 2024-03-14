4d976685032edc3631b854179f0526f22df49d01
Keffin <kevin.alemi97@gmail.com>
2024-03-14 21:06:01 +0100

    chore: basic main file for running app

--- index.ts
+++ index.ts
@@ -1,1 +1,7 @@
-console.log("Hello world");
+import { FileDetector } from "./detector/FileDetector";
+import { MasterPassword } from "./MasterPassword";
+
+const fd: FileDetector = new FileDetector();
+const m = new MasterPassword({ hash: "", salt: "", iterationCount: 1 }, fd);
+
+m.handle();
