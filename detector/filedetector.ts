566f30711aca9610ff1e7955602474bd489a57a9
Keffin <kevin.alemi97@gmail.com>
2024-03-15 17:18:45 +0100

    feat: update fd class

--- detector/filedetector.ts
+++ detector/filedetector.ts
@@ -2,48 +2,64 @@ import fs from "fs";
 import path from "path";
 import os from "os";
 
+import { AleSafeSecurity } from "../models/aleSafeTypes";
+import { AleSafeError } from "../models/AlesafeError";
+
 export class FileDetector {
+  private readonly homeDir: string = os.homedir();
+  private readonly alesafeDirName: string = ".alesafe";
+  private readonly alesafeFileName: string = ".alexp.json";
+
   // Public method for fetching the alesafe file.
   public getAlesafeFile(): string {
-    try {
-      const fetchedFiled: [string, boolean] = this.fetchFile();
-      if (fetchedFiled[1] === false) {
-        throw new Error(fetchedFiled[0]);
-      }
+    const fetchedFiled: [string, boolean] = this.fetchFile();
 
-      return fetchedFiled[0];
-    } catch (err) {
-      if (err instanceof Error) {
-        console.error(err);
-      }
-
-      return "Failed to get AlesafeFile";
+    if (fetchedFiled[1] === false) {
+      throw new AleSafeError(fetchedFiled[0]);
     }
+
+    return fetchedFiled[0];
+  }
+
+  public setupAlesafeConfig(aleSafeSecurityConfig: AleSafeSecurity): void {
+    console.log(
+      "Seems like you are missing the config needed...setting it up..."
+    );
+
+    // Create alesafe DIR
+    const alesafeDir: string = path.join(this.homeDir, this.alesafeDirName);
+    fs.mkdirSync(alesafeDir);
+    // Create alesafe JSON file
+    const alesafeJson: string = path.join(alesafeDir, this.alesafeFileName);
+
+    const fileContent = {
+      aleSafeSecurity: {
+        ...aleSafeSecurityConfig,
+      },
+      credentials: [],
+    };
+    // Write the master password details but hashed, its salt and the count
+    fs.writeFileSync(alesafeJson, JSON.stringify(fileContent, null, 2));
   }
 
   // Looks into home dir to find the file.
   private fetchFile(): [string, boolean] {
-    const homeDir: string = os.homedir();
-    const hiddenDirName: string = ".alesafe";
-    const fileName: string = ".alexp.json";
-    // .alexp.json
-
-    const hiddenDirPath: string = path.join(homeDir, hiddenDirName);
+    const hiddenDirPath: string = path.join(this.homeDir, this.alesafeDirName);
 
     if (
       !fs.existsSync(hiddenDirPath) ||
       !fs.statSync(hiddenDirPath).isDirectory()
     ) {
       return [
-        `The directory ${hiddenDirName} does not exist. Create it by running mkdir ~/.alesafe`,
+        `The directory ${this.alesafeDirName} does not exist. Create it by running mkdir ~/.alesafe`,
         false,
       ];
     }
 
-    const fp: string = path.join(hiddenDirPath, fileName);
+    const fp: string = path.join(hiddenDirPath, this.alesafeFileName);
     if (!fs.existsSync(fp)) {
       return [
-        `Filename: ${fileName} not found at ${hiddenDirName}. Create it by running touch ~/.alesafe/.test.txt`,
+        `Filename: ${this.alesafeFileName} not found at ${this.alesafeDirName}. Create it by running touch ~/.alesafe/.test.txt`,
         false,
       ];
     }
