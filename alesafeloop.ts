d3d11dcea0fc505c8a4538c5bb2a40d8baadd836
Keffin <kevin.alemi97@gmail.com>
2024-03-24 15:33:05 +0100

    chore: alesafe main loop fixes

--- alesafeloop.ts
+++ alesafeloop.ts
@@ -1,4 +1,9 @@
-import { FileDetector } from "./detector/filedetector";
+import {
+  getAlesafeFile,
+  getAleSafeFileContent,
+  isFirstRun,
+  setupAlesafeConfig,
+} from "./detector/filedetector";
 import { AleSafeManager } from "./manager/alesafemanager";
 import { AleSafeError } from "./models/AleSafeError";
 import {
@@ -9,26 +14,23 @@ import {
 import { AleSafeSecurityService } from "./security/security";
 
 export class AlesafeLoop {
-  private fd: FileDetector;
   private securityHandler: AleSafeSecurityService;
   private aleSafeManager: AleSafeManager;
 
   constructor(
-    fd: FileDetector,
     securityHandler: AleSafeSecurityService,
     aleSafeManager: AleSafeManager
   ) {
-    this.fd = fd;
     this.securityHandler = securityHandler;
     this.aleSafeManager = aleSafeManager;
   }
 
-  public getAllPw(pw: string): Credential[] {
+  public getAllCredentials(pw: string): Credential[] {
     if (!this.securityHandler.authenticate(pw)) {
       throw new AleSafeError("invalid master password supplied");
     }
 
-    const aleSafe: AleSafeFull = this.fd.getAleSafeFileContent();
+    const aleSafe: AleSafeFull = getAleSafeFileContent();
 
     const allPws: Credential[] = [];
     for (const cred of aleSafe.credentials) {
@@ -45,55 +47,52 @@ export class AlesafeLoop {
     return allPws;
   }
 
-  public getPwPlain(pw: string, website: string): string {
-    if (!this.securityHandler.authenticate(pw)) {
+  public getCredential(masterPassword: string, website: string): Credential {
+    if (!this.securityHandler.authenticate(masterPassword)) {
       throw new AleSafeError("invalid master password supplied");
     }
 
-    const aleSafe: AleSafeFull = this.fd.getAleSafeFileContent();
+    const aleSafe: AleSafeFull = getAleSafeFileContent();
 
     const credential = aleSafe.credentials.find(
       (cred) => cred.website === website
     );
 
     if (credential) {
-      return this.securityHandler.readCredentialPassword(
-        credential,
-        pw,
-        aleSafe.aleSafeSecurity
-      );
+      return {
+        website: website,
+        username: credential.username,
+        password: this.securityHandler.readCredentialPassword(
+          credential,
+          masterPassword,
+          aleSafe.aleSafeSecurity
+        ),
+      };
     }
 
-    return "no matching credentials for given webpage";
+    throw new AleSafeError("no matching credentials for given webpage");
+  }
+
+  public add(credential: Credential, masterPassword: string): void {
+    if (!this.securityHandler.authenticate(masterPassword)) {
+      throw new AleSafeError("invalid master password supplied");
+    }
+    this.aleSafeManager.addPasswordEntry(credential, masterPassword);
   }
 
   // TODO: Setup main loop
-  public handle(credentials: Credential, masterPassword: string): void {
+  public setup(masterPassword: string): void {
     // Handle first time setup
-    if (this.isFirstRun()) {
+    if (isFirstRun()) {
       // So second time user auths it will be looking at inputted PW and hashing it with the hash from file and verifying.
 
       const hashedConfig: AleSafeSecurity =
         this.securityHandler.setupMasterPassword(masterPassword);
 
-      this.fd.setupAlesafeConfig(hashedConfig);
-    }
-
-    if (!this.securityHandler.authenticate(masterPassword)) {
-      throw new AleSafeError("invalid master password supplied");
-    }
-    this.aleSafeManager.addPasswordEntry(credentials, masterPassword);
-  }
-
-  private isFirstRun(): boolean {
-    try {
-      const _ = this.fd.getAlesafeFile();
-      return false;
-    } catch (error) {
-      if (error instanceof AleSafeError) {
-        return true;
-      }
-      throw error;
+      setupAlesafeConfig(hashedConfig);
+      return;
+    } else {
+      console.log("AleSafe config already setup...");
     }
   }
 }
