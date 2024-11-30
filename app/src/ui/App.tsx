4830316194484454d6a181e98e7cf2cfe2efa17a
Keffin <kevin.alemi97@gmail.com>
2024-11-30 17:26:18 +0100

    feat: clean up app tsx

--- app/src/ui/App.tsx
+++ app/src/ui/App.tsx
@@ -1,45 +1,10 @@
-import { useEffect, useState } from "react";
 import "./App.css";
-import { AlesafeFullElectron } from "../../types";
-import { Box, Flex, Spinner } from "@chakra-ui/react";
+import { Login } from "./components/Login";
 
 function App() {
-  const [alesafeSec, setAlesafeSec] = useState<AlesafeFullElectron>();
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => {
-      // @ts-expect-error ignore
-      window.electron.getContent((r) => {
-        setAlesafeSec(r);
-        setIsLoading(false);
-      });
-    }, 1000);
-  }, []);
-
-  if (isLoading) {
-    return <Spinner size="xl" />;
-  }
-
-  if (!alesafeSec) {
-    return <div>No credentrials </div>;
-  }
-
-  const c1 = alesafeSec.credentials[0];
   return (
     <>
-      <Box
-        display="block"
-        background="tomato"
-        width="100%"
-        padding="4"
-        color="white"
-        _hover={{ bg: "green" }}
-      >
-        hellofsdfsdsdf
-      </Box>
-
-      <div>{c1.username}</div>
+      <Login />
     </>
   );
 }
